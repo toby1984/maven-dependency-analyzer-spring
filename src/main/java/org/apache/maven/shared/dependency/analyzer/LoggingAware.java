fdbff1dea3125947556cffb917536bbc29877203
Tobias Gierke <tobias.gierke@code-sourcery.de>
2010-05-09 02:01:34 +0200

    more pom.xml fixes , added license

--- src/main/java/org/apache/maven/shared/dependency/analyzer/LoggingAware.java
+++ src/main/java/org/apache/maven/shared/dependency/analyzer/LoggingAware.java
@@ -2,6 +2,13 @@ package org.apache.maven.shared.dependency.analyzer;
 
 import org.apache.maven.plugin.logging.Log;
 
+/**
+ * Helper interface to inject the Maven logger at runtime.
+ *
+ * TODO: The Maven core probably has a better way of doing this...find out how.
+ * 
+ * @author tobias.gierke@code-sourcery.de
+ */
 public interface LoggingAware {
 
 	public void setLog(Log log);
